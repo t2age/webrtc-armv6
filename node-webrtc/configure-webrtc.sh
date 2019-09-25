7109757e2cac7ee2159cf129b6a318a2c25e913d
t2age <55773266+t2age@users.noreply.github.com>
2019-09-25 17:04:16 +0900

    Add files via upload

---
+++ node-webrtc/configure-webrtc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+set -v
+
+export PATH=$DEPOT_TOOLS:$PATH
+
+cd ${SOURCE_DIR}
+
+if [ "$TARGET_ARCH" == "arm" ]; then
+  python build/linux/sysroot_scripts/install-sysroot.py --arch=arm
+elif [ "$TARGET_ARCH" == "armv6" ]; then
+  python build/linux/sysroot_scripts/install-sysroot.py --arch=arm
+elif [ "$TARGET_ARCH" == "arm64" ]; then
+  python build/linux/sysroot_scripts/install-sysroot.py --arch=arm64
+else
+  python build/linux/sysroot_scripts/install-sysroot.py --arch=amd64
+fi
+gn gen ${BINARY_DIR} "--args=${GN_GEN_ARGS}"
